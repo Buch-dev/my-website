cc45d2a644d1057e0feb4b7a9092b3b0ddc2a907
Daniel Okafor <bucheed@gmail.com>
2025-05-29 14:19:03 +0100

    fixed deployment bug

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
 import CTA from "@/app/components/CTA";
-import { link } from "fs";
 
 const portfolioData = [
   {
